8fbf1dfe38e3f60017ea6d995b48046f4cf71bcc
Yuvan12128 <yuvan12128@gmail.com>
2025-08-24 18:41:29 +0530

    update python notice

--- main.py
+++ main.py
@@ -1,28 +1,4 @@
-#Identifiers - Variables - reference Variables
-
-doorNo = 15 # = --> Assignment operator
-
-print(doorNo)
-
-# alphabets - small / capital 
-# number are allowed
-# Underscore is allowed
-# No other special character is allowed 
-# don't start to numbers
-
-tamil_1=90
-print(tamil_1)
-tamil2=98
-print(tamil2)
-TAMIL=40
-print(TAMIL)
-# 1TAMIL=40
-# print(1TAMIL) 
-
-_tamil=200  # private variable
-print(_tamil)
-
-__english=40 #double underscore 
-print(__english)
-_maths_=101
-print(_math_)
+no = 0o786
+print(no)
+no = 0O789
+print(no)
