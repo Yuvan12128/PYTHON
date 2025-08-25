eafd35931b21aa829d8b6c12ff1a869dcdd427ca
Yuvan12128 <yuvan12128@gmail.com>
2025-08-25 19:42:50 +0530

    update int code

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-no = 0o786
-print(no)
-no = 0O789
-print(no)
+no1=10  #assignment operator 
+no2=20
+print(no1<no2) #10<20   True
+print(no1>no2) #10>20   False
+print(no1==no2) #comparison equal
+print(no1!=no2)
+
+present = True
+print(present)
+print(type(present))
+
+print(True+True)
