9aede83a97bb0a864a3a970413cec427c4d7b050
Yuvan12128 <yuvan12128@gmail.com>
2025-09-05 21:18:14 +0530

    update

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print(5 or 4)
+def biggest(n1,n2): # function definition
+    if n1>n2:
+        print(n1,'is greater')
+    else:
+        print(n2,'is greater' )
+
---
+++ main2.py
@@ -0,0 +1,7 @@
+import main
+no1 = int(input('enter number : '))
+
+no2= int(input('enter number : '))
+
+main.biggest(no1,no2) #function calling
+#arguments for given function
