12c7e6cdbe333e269c0488981e465a6af5d6821c
Yuvan12128 <yuvan12128@gmail.com>
2025-09-27 10:16:25 +0530

    update

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-email=input('enter you email : ')
-count=0
-length = len(email)
-i=0
-while i<length:
-    if email[i]>='a' and email[i]<='z':
-        print(email[i])
-        count+=1
-    i+=1
+import math
 
-print(count)
+print(math.gcd(120,200))
+no1, no2=120,200
+div =2
+if no1<no2:
+    small=no1
+elif no2<no1:
+    small=no2
+while div<=small:
+    if no1%div==0 and no2%div==0:
+        last=div
+    div+=1
+else:
+        print(last)
